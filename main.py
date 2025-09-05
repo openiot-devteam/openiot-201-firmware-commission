a5bf464d2f6b6abb9e23711eb2dd9768c7ed09e8
nano-2-ly <dong991207@unist.ac.kr>
2025-09-05 17:27:15 +0900

    update: pairing API를 재민님 AWS URL 로 변경

--- main.py
+++ main.py
@@ -487,11 +487,11 @@ def send_commission_request(server_info):
         print(f"클라이언트 IP: {client_ip}")
         
         # 서버 URL 구성
-        server_url = f"http://{server_info['ip']}:{server_info['port']}/commission"
+        server_url = f"http://{server_info['ip']}:{server_info['port']}/devices/device-pairing"
         
         # 요청 데이터 준비
         request_data = {
-            "client_ip": client_ip
+            "deviceIp": client_ip
         }
         
         # API 요청 보내기
